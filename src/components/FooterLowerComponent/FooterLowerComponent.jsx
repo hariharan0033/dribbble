5bfa56c3cd80e82b445e594ddb74c16d76c5013d
hariharan0033 <hariharan00332810@gmail.com>
2024-06-22 14:54:16 +0530

    Added FooterLowerComponent

--- src/components/FooterLowerComponent/FooterLowerComponent.jsx
+++ src/components/FooterLowerComponent/FooterLowerComponent.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './FooterLowerComponent.css';
 
 const FooterLowerComponent = () => {
   return (
-    <div>FooterLowerComponent</div>
+    <React.Fragment>
+      <div className="footer-lower">
+      <div className="footer-lower-left">
+          <ul>
+            <li>&copy; 2024 Dribbble</li>
+            <li><a href="">Terms</a></li>
+            <li><a href="">Privacy</a></li>
+            <li><a href="">Cookies</a></li>
+          </ul>
+      </div>
+
+      <div className="footer-lower-right">
+          <ul>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Designers</a></li>
+            <li><a href="">Freelancers</a></li>
+            <li><a href="">Tags</a></li>
+            <li><a href="">Places</a></li>
+            <li><a href="">Resources</a></li>
+          </ul>
+      </div>
+      </div>
+    </React.Fragment>
   )
 }
 
