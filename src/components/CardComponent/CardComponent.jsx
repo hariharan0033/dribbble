ed17fb7a5b1aa45de5d5b23f9d0d5cd6c8ab60e7
hariharan0033 <hariharan00332810@gmail.com>
2024-06-23 12:07:28 +0530

    Added like & save feature

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -9,26 +9,36 @@ import { HiBookmark } from "react-icons/hi2";
 
 const CardComponent = ({card}) => {
     const [hoverClass , setHoverClass] = useState('');
+    const [saved , setSaved] = useState(false)
+    const [like , setLike] = useState(card.likecount)
 
-    const handleMouseEnter = () => {
-        setClassName('hover');
+      const handleMouseEnter = () => {
+        setHoverClass('hover');
       };
     
       const handleMouseLeave = () => {
-        setClassName('');
+        setHoverClass('');
+      };
+      const handleSaveClick = () => {
+        setSaved(!saved);
+        
+      };
+      const handleLikeClick = () => {
+        card.isliked ? setLike(like - 1) : setLike(like + 1) ;
+        card.isliked = !card.isliked ;
       };
 
   return (
     <React.Fragment>
         <div className="card-container">
-            <div className="img-container">
+            <div className="img-container"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <img src={card.imagelink} alt="" className='card-image'/>
                 <div className={`img-hover-details ${hoverClass}`}>
                     <h3>{card.name}</h3>
                     <div className="img-actions">
-                      <div className="actions"><HiOutlineBookmark /></div>
-                      {/* <div className="actions"><HiBookmark /></div> */}
-                      <div className="actions"><RiHeart3Line /></div>
+                      <div className="actions" onClick={handleSaveClick}>{saved ? <HiBookmark /> : <HiOutlineBookmark />}</div>
+
+                      <div className="actions" onClick={handleLikeClick}>{card.isliked ? <GoHeartFill className='liked' /> : <RiHeart3Line />}</div>
                     </div>
                 </div>
             </div>
@@ -41,8 +51,9 @@ const CardComponent = ({card}) => {
                 </div>
                 <div className="img-details">
                     <div className="likes">
-                        <GoHeartFill className='like'/>
-                        <span>{card.likecount}</span>
+                        {card.isliked?<GoHeartFill className="like liked"  onClick={handleLikeClick}/>
+                                     :<GoHeartFill className="like"  onClick={handleLikeClick}/>}
+                        <span>{like}</span>
                     </div>
                     <div className="views">
                         <TbEyeFilled />
